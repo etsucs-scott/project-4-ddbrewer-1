98c307a120b683adee2a5f4d20d79c1a5ff7a48b
ddbrewer <brewerdd@etsu.edu>
2026-04-17 00:27:24 -0400

    Tested in Program.cs.

--- RPGPartyBuilder.App/Program.cs
+++ RPGPartyBuilder.App/Program.cs
@@ -1,3 +1,10 @@
 ﻿using System;
+using RPGPartyBuilder.App.Models;
 
-Console.WriteLine("Hello, World!");
+Character c1 =  new Character("Dakota", "Warrior", "Tank", 1, 100, 50, 20, 10);
+
+Party party = new Party("My First Party");
+party.Members.Add(c1);
+
+Console.WriteLine(party.PartyName);
+Console.WriteLine(party.Members[0]);
